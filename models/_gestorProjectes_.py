86005e0cd2c97bd3e0f4a9974c23644b64a72cdb
aitorsansal <aitorsansal@gmail.com>
2025-05-15 15:13:25 +0200

    refactor: remove unused onchange method and logging from GestorDeProjectes

--- models/_gestorProjectes_.py
+++ models/_gestorProjectes_.py
@@ -18,14 +18,8 @@ class GestorDeProjectes(models.Model):
     worksDone = fields.Text('Obres Realitzades')
     hoursWorked = fields.Float('Hores Treballades')
 
-    @api.onchange('end_date')
-    def _check_expiration(self):
-        if self.end_date and self.end_date < fields.Date.today():
-            self.state = 'expired'
-
     @api.model
     def create(self, vals):
-
         if 'creation_date' not in vals or not vals['creation_date']:
             vals['creation_date'] = fields.Date.today()
         # Set default state if not provided
@@ -35,35 +29,12 @@ class GestorDeProjectes(models.Model):
         # Call the super method to create the record
         record = super(GestorDeProjectes, self).create(vals)
 
-        # Custom logic after record creation (if needed)
-        # Example: Log a message
-        self.env['ir.logging'].create({
-            'name': 'Gestor Projectes',
-            'type': 'server',
-            'level': 'info',
-            'message': f"Nou projecte creat amb ID {record.id}",
-            'path': 'models/_gestorProjectes_.py',
-            'line': 'create',
-            'func': 'create',
-        })
-
         return record
     
     @api.onchange('state')
     def _onchange_state(self):
         if self.state == 'completed':
             self.end_date = fields.Date.today()
-            # Perform some action when the state is changed to completed
-            # Example: Log a message
-            self.env['ir.logging'].create({
-                'name': 'Gestor Projectes',
-                'type': 'server',
-                'level': 'info',
-                'message': f"Projecte amb ID {self.id} marcat com completat.",
-                'path': 'models/_gestorProjectes_.py',
-                'line': '_onchange_state',
-                'func': '_onchange_state',
-            })
         elif self.state == 'inProgress':
             self.start_date = fields.Date.today()
 
